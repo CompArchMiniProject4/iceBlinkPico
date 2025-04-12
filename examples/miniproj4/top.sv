9919b90cf7437f118e0655ab9b54881c09a1e3e1
agathaford <145080316+agathaford@users.noreply.github.com>
2025-04-12 16:16:50 -0400

    Update top.sv for leds

--- examples/miniproj4/top.sv
+++ examples/miniproj4/top.sv
@@ -1,12 +1,14 @@
 
 module top(input  logic        clk, reset, 
            output logic [31:0] WriteData, DataAdr, 
-           output logic        MemWrite);
+           output logic        MemWrite),
+           output logic [3:0] leds,
+           output logic [31:0] pwm_out;
 
 logic [31:0] ReadData;
 
 rVMultiCycle rvMultiCycle(clk, reset, ReadData, DataAdr, MemWrite, WriteData);
-mem mem(clk, MemWrite, DataAdr, WriteData, ReadData);
+           mem mem(clk, MemWrite, DataAdr, WriteData, ReadData, leds, pwm_out);
 
  
 endmodule
