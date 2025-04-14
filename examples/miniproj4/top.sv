8639be504de5c54a3f6b3d9c621623b8b97722ce
agathaford <145080316+agathaford@users.noreply.github.com>
2025-04-14 10:44:39 -0400

    Change mem to memory top.sv

--- examples/miniproj4/top.sv
+++ examples/miniproj4/top.sv
@@ -8,6 +8,6 @@ module top(input  logic        clk, reset,
   logic [31:0] ReadData;
 
   rVMultiCycle rvMultiCycle(clk, reset, ReadData, DataAdr, MemWrite, WriteData);
-  mem mem(clk, MemWrite, DataAdr, WriteData, ReadData, leds, pwm_out);
+  memory memory(clk, MemWrite, DataAdr, WriteData, ReadData, leds, pwm_out);
 
 endmodule
