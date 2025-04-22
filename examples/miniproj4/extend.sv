51e624c71ca35ae20350a78539f7f7d4d3c44d23
agathaford <145080316+agathaford@users.noreply.github.com>
2025-04-22 11:03:23 -0400

    Workaround for iverilog error extend.sv

--- examples/miniproj4/extend.sv
+++ examples/miniproj4/extend.sv
@@ -1,19 +1,27 @@
+module extend (
+    input logic [31:0] instr,
+    input logic [2:0] ImmSrc,
+    output logic [31:0] ImmExt
+);
 
-module extend (input logic [31:0] instr, input logic [2:0] ImmSrc, output logic [31:0] ImmExt);
+    // Intermediate immediate values
+    logic [31:0] imm_I, imm_S, imm_B, imm_J, imm_U;
 
-	always_comb begin
-		case(ImmSrc) //controller produces immsrc signal
-			//I
-			3'b000: ImmExt = {{20{instr[31]}}, instr[31:20]};
-			//S
-			3'b001: ImmExt = {{20{instr[31]}}, instr[31:25], instr[11:7]};
-			//B
-			3'b010: ImmExt = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
-			//J
-			3'b011: ImmExt = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
-			//U
-			3'b100: ImmExt = {instr[31:12], 12'b0};
-			default: ImmExt = 32'bx; // undefined ? ?
-		endcase
-	end
-endmodule 
+    assign imm_I = {{20{instr[31]}}, instr[31:20]};
+    assign imm_S = {{20{instr[31]}}, instr[31:25], instr[11:7]};
+    assign imm_B = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
+    assign imm_J = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
+    assign imm_U = {instr[31:12], 12'b0};
+
+    always_comb begin
+        case (ImmSrc)
+            3'b000: ImmExt = imm_I;
+            3'b001: ImmExt = imm_S;
+            3'b010: ImmExt = imm_B;
+            3'b011: ImmExt = imm_J;
+            3'b100: ImmExt = imm_U;
+            default: ImmExt = 32'bx;
+        endcase
+    end
+
+endmodule
