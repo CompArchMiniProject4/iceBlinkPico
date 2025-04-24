fb8399e9adea0e74d5f0eb0da4d2884d41c63038
Michaela Fox <mfox@olin.edu>
2025-04-24 12:06:26 -0400

    Change to synchronous reset

--- examples/miniproj4/flopr.sv
+++ examples/miniproj4/flopr.sv
@@ -1,16 +1,13 @@
-// for initial datapath file
-
-module flopr #(parameter WIDTH = 8) (
-    input  logic             clk,    // Clock input
-    input  logic             reset,  // Asynchronous reset (active-high)
-    input  logic [WIDTH-1:0] d,      // Data input
-    output logic [WIDTH-1:0] q       // Data output
+module flopr #(parameter WIDTH = 32) (
+    input  logic              clk,
+    input  logic              reset,    // Synchronous reset
+    input  logic [WIDTH-1:0]  d,
+    output logic [WIDTH-1:0]  q
 );
-
-    always_ff @(posedge clk, posedge reset) begin
-        if (reset)  q <= '0;    // Clear all bits on reset
-        else        q <= d;     // Update on every clock edge
+    always_ff @(posedge clk) begin
+        if (reset)
+            q <= '0;
+        else
+            q <= d;
     end
-
 endmodule
-
