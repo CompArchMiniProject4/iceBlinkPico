1abd3344645e8c0a708c80cb28ce247ad6d177fb
agathaford <145080316+agathaford@users.noreply.github.com>
2025-04-24 22:08:19 -0400

    Add back enable if statement flopenr.sv

--- examples/miniproj4/flopenr.sv
+++ examples/miniproj4/flopenr.sv
@@ -1,14 +1,15 @@
-module flopenr #(parameter WIDTH = 32) (
-    input  logic              clk,
-    input  logic              reset,    // Synchronous reset
-    input  logic             en,
-    input  logic [WIDTH-1:0]  d,
-    output logic [WIDTH-1:0]  q
+module flopenr #(parameter WIDTH = 8) (
+    input  logic             clk,   // Clock input
+    input  logic             reset, // Asynchronous reset (active-high)
+    input  logic             en,    // Enable input (active-high)
+    input  logic [WIDTH-1:0] d,     // Data input
+    output logic [WIDTH-1:0] q      // Data output
 );
-    always_ff @(posedge clk) begin
-        if (reset)
-            q <= '0;
-        else
-            q <= d;
+
+    always_ff @(posedge clk, posedge reset) begin
+        if (reset)  q <= '0;         // Async reset to all zeros
+        else if (en) q <= d;         // Update on enable
+        // No else: retains value when not enabled
     end
+
 endmodule
